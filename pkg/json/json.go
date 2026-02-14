ff0a44a85bda0bb975825d478db89c464c36a539
Yaroslav <zeronationday@proton.me>
2026-02-14 22:42:27 +0300

    fix(json): do not ignore errors in write method

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -2,6 +2,7 @@ package json
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 )
 
@@ -9,7 +10,9 @@ func Write(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
-	json.NewEncoder(w).Encode(data)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		slog.Error("failed to encode json response", "error", err)
+	}
 }
 
 func Read(r *http.Request, data any) error {
